b0efe2403c5b2c6a0ca9371cc545b523f1b940f6
Nathan Schwarz <nathan@notwhite.ro>
2014-12-28 22:04:38 +0100

    [tests] Retrieve test data on its own

--- test/test_sql.py
+++ test/test_sql.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 import sql
+import sqlite3
 
 class TestSQL(unittest.TestCase):
     def test_open_fail(self):
@@ -8,47 +9,46 @@ class TestSQL(unittest.TestCase):
         self.assertTrue(self.db.db == None)
 
     def test_open(self):
-        self.db.open()
+        self.db.open(self.db_file)
         self.assertTrue(self.db.db != None)
 
     def test_get_paper(self):
+        to_check = 'Dummy paper 01'
         self.db.open()
-        dummy = {
-                'Authors': 'Dummy Author Sr., Dummy Author Jr.',
-                'Count_proposed': 4,
-                'Discussion': '',
-                'Submitters': 'cspaperbot, dummyuser',
-                'Title': 'Dummy paper 01',
-                'Link': 'https://link.to/a/dummy/paper.pdf',
-                'Abstract': 'Very short and undescriptive abstract of dummy paper 01',
-                'Proposed_current_vote': 1
-                }
-        from_db = self.db.get_paper('Dummy paper 01')
-        self.assertTrue(dummy == dict(from_db))
+        from_db = self.db.get_paper(to_check)
+
+        self.test_cursor.execute("SELECT * FROM papers WHERE Title LIKE '{}'".format(to_check))
+        dummy = self.test_cursor.fetchone()
+
+        self.assertTrue(dummy == from_db)
 
     def test_get_user(self):
+        to_check = 'cspaperbot'
         self.db.open()
-        dummy = {
-                'Name': 'cspaperbot',
-                'Discussed_submissions': 'Dummy paper 01',
-                'Discussion_comments': 0,
-                'Submissions': 1
-                }
-        from_db = self.db.get_user('cspaperbot')
-        self.assertTrue(dummy == dict(from_db))
+        from_db = self.db.get_user(to_check)
+
+        self.test_cursor.execute("SELECT * FROM users WHERE Name LIKE '{}'".format(to_check))
+        dummy = self.test_cursor.fetchone()
+
+        self.assertTrue(dummy == from_db)
 
     def test_get_author(self):
+        to_check = 'Paul Paper-Leecher'
         self.db.open()
-        dummy = {
-                'Name': 'Paul Paper-Leecher',
-                'Homepage': 'https://paper-leecher.com/',
-                'CV': 'https://paper-leecher.com/cv.pdf'
-                }
-        from_db = self.db.get_author('Paul Paper-Leecher')
-        self.assertTrue(dummy == dict(from_db))
+        from_db = self.db.get_author(to_check)
+
+        self.test_cursor.execute("SELECT * FROM authors WHERE Name LIKE '{}'".format(to_check))
+        dummy = self.test_cursor.fetchone()
+
+        self.assertTrue(dummy == from_db)
 
     def setUp(self):
         self.db = sql.Database()
+        self.db_file = 'db/papers.db'
+        self.test_db = sqlite3.connect(self.db_file)
+        self.test_db.row_factory = sqlite3.Row
+        self.test_cursor = self.test_db.cursor()
+        pass
 
     def tearDown(self):
         pass
