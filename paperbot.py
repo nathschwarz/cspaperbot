14252183f9f9c43531a2a5bd1e1ff132d0fd8375
Nathan Schwarz <nathan@notwhite.ro>
2014-12-28 20:18:27 +0100

    [bot] Opening of config-file and logging in to reddit

---
+++ paperbot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import praw
+import yaml
+import logging
+
+#logging defaults
+logging.basicConfig(filename='paperbot.log', level=logging.ERROR)
+
+def load_config(conf_file='cspaperbot.conf'):
+    try:
+        with open(conf_file, 'r') as f:
+            return yaml.load(f)
+    except Exception as e:
+        logging.error(e)
+
+def login(user_agent, username, password):
+    try:
+        r = praw.Reddit(user_agent = user_agent)
+        r.login(username, password)
+        logging.info('Login successful')
+        return r
+    except Exception as e:
+        logging.error(e)
+
+def main():
+    conf = load_config()
+    r = login(conf['user_agent'], conf['username'], conf['password'])
+
+if __name__ == "__main__":
+    main()
