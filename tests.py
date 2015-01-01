56d790878b10a4c74176750676929c0eb0f3de08
Nathan Schwarz <nathan@notwhite.ro>
2015-01-01 22:56:27 +0100

    Remove more unnecessary files

--- tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#Run unit tests
-
-import unittest
-
-if __name__ == "__main__":
-    testsuite = unittest.TestLoader().discover('./test/')
-    unittest.TextTestRunner(verbosity=1).run(testsuite)
-
