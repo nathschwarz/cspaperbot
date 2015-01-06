0f729f276bdf0890e986c5106e210a894632d14a
Nathan Schwarz <nathan@notwhite.ro>
2015-01-06 21:05:19 +0000

    Use a named logger

--- paperbot.py
+++ paperbot.py
@@ -47,6 +47,7 @@ today_plus_two_weeks = str(datetime.date.today() + datetime.timedelta(weeks = 2)
 r = None
 conf = None
 db = sql.Database()
+logger = None
 
 def load_config():
     """Loads configuration from 'cspaperbot.conf' and returns it."""
@@ -55,7 +56,7 @@ def load_config():
         with open(conf_file, 'r') as f:
             conf = yaml.load(f)
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
 
 def write_config():
     """Writes configuration to 'cspaperbot.conf'."""
@@ -63,7 +64,7 @@ def write_config():
         with open(conf_file, 'w') as f:
             yaml.dump(conf, f, default_flow_style=False)
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
 
 def login():
     """Logs in to reddit with given username and password, returns reddit-instance."""
@@ -71,20 +72,20 @@ def login():
     try:
         r = praw.Reddit(user_agent = user_agent)
         r.login(conf['username'], conf['password'])
-        logging.info('Login successful')
+        logger.info('Login successful')
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
 
 def reply_to(comment, body):
     """Reply to given comment with given text. Appends postfix automatically."""
-    logging.info('Commented on ' + comment.id + ":\n" + body)
+    logger.info('Commented on ' + comment.id + ":\n" + body)
     comment.reply(body + '  \n' + postfix)
 
 def create_thread(title, body):
     return r.submit(conf['subreddit'], title + str(conf['paper_round']), body + ' \n' + postfix)
 
 def create_voting_thread():
-    logging.info('Created voting thread')
+    logger.info('Created voting thread')
     voting_thread = create_thread(voting_title, voting_body)
     if conf['moderator'] == True:
         voting_thread.sticky()
@@ -92,7 +93,7 @@ def create_voting_thread():
     return voting_thread
 
 def create_discussion_thread():
-    logging.info('Created discussion thread')
+    logger.info('Created discussion thread')
     list_submissions, top_paper = parse_voting_thread()
     if list_submissions:
         discussion_thread = create_thread(discussion_title, discussion_body.format(top_paper.author.name, today, top_paper.body))
@@ -104,14 +105,14 @@ def create_discussion_thread():
         db.upsert_paper(top_paper)
         return discussion_thread
     else:
-        logging.error('Empty response from parsed voting thread - no participation or error in parsing.')
+        logger.error('Empty response from parsed voting thread - no participation or error in parsing.')
 
 def add_wiki_page(table, link):
     subreddit = 'cspaperbot'
     r.edit_wiki_page(subreddit, 'voting/'+today, '#Voting result of {0}\n\n[Discussion]({1})\n\n{2}'.format(today, link, table))
     r.edit_wiki_page(subreddit, 'index', '{0}  \n[Voting of {1}](/r/{2}/wiki/voting/{1}/)'.
                 format(r.get_wiki_page(subreddit, 'index').content_md, today, subreddit))
-    logging.info('Uploaded wiki-pages for new voting')
+    logger.info('Uploaded wiki-pages for new voting')
 
 def submissiontable(list_submissions):
     table = 'Karma count | Submitter | Paper title | Link to paper  \n ---|---|---|---  \n'
@@ -127,7 +128,7 @@ def submissiontable(list_submissions):
 def parse_comment_to_paper(comment):
     """Parses given comment body into a dictionary."""
     if 'WITHDRAWN' in comment:
-        logging.info('Withdrawn submission:' + comment)
+        logger.info('Withdrawn submission:' + comment)
         return None
     try:
         paper = {}
@@ -135,18 +136,18 @@ def parse_comment_to_paper(comment):
         paper['Authors'] = re.findall(regex_author_list, re.search(regex_authors, comment).group(1))
         paper['Link'] = re.search(regex_link, comment).group(1)
         paper['Abstract'] = re.search(regex_abstract, comment).group(1)
-        logging.info('Paper submission: ' + str(paper))
+        logger.info('Paper submission: ' + str(paper))
         return paper
     except Exception as e:
-        logging.error('Parse error: ' + comment, e)
+        logger.error('Parse error: ' + comment, e)
 
 def process_comment(comment):
-    logging.info('Processing comment')
+    logger.info('Processing comment')
     paper = parse_comment_to_paper(comment.body)
     if paper:
         query_paper = db.find_paper(paper['Title'])
         if query_paper:
-            logging.info('Paper already submitted:' + str(query_paper))
+            logger.info('Paper already submitted:' + str(query_paper))
             query_paper['Count_proposed'] += 1
             #This line is awful. Sadly nosqlite doesn't support sets
             if comment.author.name not in paper['Submitters']:
@@ -158,7 +159,7 @@ def process_comment(comment):
             else:
                 reply_to(comment, 'This paper was already discussed [here]('+query_paper['Discussion']+').\nThanks for your suggestion.')
         else:
-            logging.info('New paper')
+            logger.info('New paper')
             paper['Count_proposed'] = 1
             paper['Discussion'] = ''
             paper['Submitters'] = [comment.author.name]
@@ -169,7 +170,7 @@ def process_comment(comment):
             return paper
 
 def parse_voting_thread():
-    logging.info('Parsing voting thread')
+    logger.info('Parsing voting thread')
     voting_thread = r.get_submission(submission_id = conf['current_voting_thread'])
     if conf['moderator']:
         voting_thread.unsticky()
@@ -180,11 +181,12 @@ def parse_voting_thread():
     return list_submissions, comments[0]
 
 def parse_pms():
+    logger.info('Parsing PMs')
     pms = r.get_unread()
     for pm in pms:
         author = pm.author.name
         if 'reply' in pm.subject:
-            logging.info('Reply to comment or post logged')
+            logger.info('Reply to comment or post logged')
         if 'subscribe' in pm.body:
             reply = "Thanks, you're now "
             if 'unsubscribe' in pm.body:
@@ -203,20 +205,20 @@ def parse_pms():
                 if 'voting' in pm.body:
                     conf['voting_subscribers'].add(author)
                     reply += "voting"
-            logging.info('Subscriptions: ' + author + ' - '+reply)
+            logger.info('Subscriptions: ' + author + ' - '+reply)
             r.send_message(pm.author.name, 'Submission settings', reply)
         else:
-            logging.info('Unparseable message from ' + author + ', forwarding')
+            logger.info('Unparseable message from ' + author + ', forwarding')
             r.send_message('nath_schwarz', 'Unparseable message from ' + author, pm.body)
         pm.mark_as_read()
 
 def send_notifications(link, title, recipients):
-    logging.info('Sending notification to subscribers')
+    logger.info('Sending notification to subscribers')
     for recipient in recipients:
         r.send_message(recipient, title, link)
 
 def execute_actions():
-    logging.info('Checking for actions')
+    logger.info('Checking for actions')
     parse_pms()
     if today == conf['next_voting_date']:
         thread = create_voting_thread()
@@ -235,12 +237,14 @@ def main():
     parser.add_argument("--stdout", action="store_true", help="print log output to stdout")
     args = parser.parse_args()
 
+    global logger
     if args.verbose:
         logging.basicConfig(level = logging.INFO)
     else:
         logging.basicConfig(level = logging.ERROR)
     if not args.stdout:
         logging.basicConfig(filename = 'cspaperbot.log')
+    logger = logging.getLogger('cspaperbot')
 
     global conf, r
     load_config()
